66fdce0b5a047ba9896041c02a1f6a769e30d7df
Nguyễn Tú Anh <ntanhtm@gmail.com>
2025-06-09 17:49:28 +0700

    Build Mon Jun  9 17:49:23 +07 2025: tinh chinh

--- whiteneuron/base/utils.py
+++ whiteneuron/base/utils.py
@@ -35,15 +35,6 @@ def user_badge_callback(request):
 def group_badge_callback(request):
     return ""
 
-def notification_badge_callback(request):
-    c= Notification.objects.filter(
-        user=request.user,
-        is_read=False,
-        created_at__gte= timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)).count()
-    if c==0:
-        return ''
-    return f"+{c}"
-
 def permission_callback(request):
     return False
 
