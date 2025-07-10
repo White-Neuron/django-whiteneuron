55201120a4d4a15a8c6fa8e3a242a0fd442a1c8d
Nguyễn Tú Anh <ntanhtm@gmail.com>
2025-07-10 16:48:53 +0700

    Build Thu Jul 10 16:48:46 +07 2025: toast

--- whiteneuron/static/base/js/whiteneuron.js
+++ whiteneuron/static/base/js/whiteneuron.js
@@ -11,31 +11,51 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-// WebSocket connection for notifications
-const ws = new WebSocket("ws://" + window.location.host + "/ws/notifications/");
-ws.onmessage = function(event) {
-    const data = JSON.parse(event.data);
-    const toast = document.getElementById("toast-container");
-    if (toast) {
+function toast_(message, type = "info", title = "Notification") {
+    const toastContainer = document.getElementById("toast-container");
+    if (toastContainer) {
         const toastItem = document.createElement("div");
-        if (data.type === "success") {
-            toastItem.className = "ui-alert ui-alert-success";
-        } else if (data.type === "error") {
-            toastItem.className = "ui-alert ui-alert-error";
-        } else if (data.type === "info") {
-            toastItem.className = "ui-alert ui-alert-info";
-        } else if (data.type === "warning") {
-            toastItem.className = "ui-alert ui-alert-warning";
-        }
+        toastItem.className = `ui-alert ui-alert-${type}`;
         toastItem.innerHTML = `
-            <span>${data.title}</span>
-            <p>${data.content}</p>
+            <span>${title}</span>
+            <p>${message}</p>
         `;
-        toast.appendChild(toastItem);
+        toastContainer.appendChild(toastItem);
         
         // Automatically remove the toast after 5 seconds
         setTimeout(() => {
             toastItem.remove();
         }, 5000);
     }
+}
+
+// WebSocket connection for notifications
+const ws = new WebSocket("ws://" + window.location.host + "/ws/notifications/");
+ws.onmessage = function(event) {
+    const data = JSON.parse(event.data);
+    toast_(data.content, data.type, data.title);
 };
+    // const toast = document.getElementById("toast-container");
+    // if (toast) {
+    //     const toastItem = document.createElement("div");
+    //     if (data.type === "success") {
+    //         toastItem.className = "ui-alert ui-alert-success";
+    //     } else if (data.type === "error") {
+    //         toastItem.className = "ui-alert ui-alert-error";
+    //     } else if (data.type === "info") {
+    //         toastItem.className = "ui-alert ui-alert-info";
+    //     } else if (data.type === "warning") {
+    //         toastItem.className = "ui-alert ui-alert-warning";
+    //     }
+    //     toastItem.innerHTML = `
+    //         <span>${data.title}</span>
+    //         <p>${data.content}</p>
+    //     `;
+    //     toast.appendChild(toastItem);
+        
+    //     // Automatically remove the toast after 5 seconds
+    //     setTimeout(() => {
+    //         toastItem.remove();
+    //     }, 5000);
+    // }
+// };
