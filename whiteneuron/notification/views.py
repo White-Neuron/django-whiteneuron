fc14f475a2f5998e4e2c49995b14c7e5e9610d29
Nguyễn Tú Anh <ntanhtm@gmail.com>
2025-07-21 14:47:02 +0700

    Build Mon Jul 21 14:46:50 +07 2025: notify_admin(message, type="info", obj_link=None, action=None)

--- whiteneuron/notification/views.py
+++ whiteneuron/notification/views.py
@@ -5,7 +5,7 @@ from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 from django.utils.translation import gettext_lazy as _
 
-def notify_admin(message, type="info", obj_link=None):
+def notify_admin(message, type="info", obj_link=None, action=None):
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(
         "admin_notifications",
@@ -16,6 +16,7 @@ def notify_admin(message, type="info", obj_link=None):
                 "content": message,
                 "type": type,
                 'obj_link': obj_link,
+                'action': action,
             }
         }
     )
