c321a32f386a823e495706ef24b579beb0d0987b
Nguyễn Tú Anh <ntanhtm@gmail.com>
2025-07-22 09:50:39 +0700

    Build Tue Jul 22 09:50:32 +07 2025: wss & ws

--- whiteneuron/static/base/js/whiteneuron.js
+++ whiteneuron/static/base/js/whiteneuron.js
@@ -30,32 +30,9 @@ function toast_(message, type = "info", title = "Notification") {
 }
 
 // WebSocket connection for notifications
-const ws = new WebSocket("ws://" + window.location.host + "/ws/notifications/");
+const wsProtocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
+const ws = new WebSocket(wsProtocol + window.location.host + "/ws/notifications/");
 ws.onmessage = function(event) {
     const data = JSON.parse(event.data);
     toast_(data.content, data.type, data.title);
 };
-    // const toast = document.getElementById("toast-container");
-    // if (toast) {
-    //     const toastItem = document.createElement("div");
-    //     if (data.type === "success") {
-    //         toastItem.className = "ui-alert ui-alert-success";
-    //     } else if (data.type === "error") {
-    //         toastItem.className = "ui-alert ui-alert-error";
-    //     } else if (data.type === "info") {
-    //         toastItem.className = "ui-alert ui-alert-info";
-    //     } else if (data.type === "warning") {
-    //         toastItem.className = "ui-alert ui-alert-warning";
-    //     }
-    //     toastItem.innerHTML = `
-    //         <span>${data.title}</span>
-    //         <p>${data.content}</p>
-    //     `;
-    //     toast.appendChild(toastItem);
-        
-    //     // Automatically remove the toast after 5 seconds
-    //     setTimeout(() => {
-    //         toastItem.remove();
-    //     }, 5000);
-    // }
-// };
